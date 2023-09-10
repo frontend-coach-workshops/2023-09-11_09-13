1067c518de31478a826caeefdefb613ca7963088
Joe Ray Gregory <joe@may17.de>
2023-09-11 00:20:39 +0200

    Add task 8

---
+++ src/components/ColorBox.vue
@@ -0,0 +1,106 @@
+<template>
+  <nav>
+    <button
+      class="btn btn-a"
+      :class="{ 'btn-active': activeLetter === 'A' }"
+      v-on:click="activeLetter = 'A'"
+    >
+      A
+    </button>
+    <button
+      class="btn btn-b"
+      :class="{ 'btn-active': activeLetter === 'B' }"
+      v-on:click="activeLetter = 'B'"
+    >
+      B
+    </button>
+    <button
+      class="btn btn-c"
+      :class="{ 'btn-active': activeLetter === 'C' }"
+      v-on:click="activeLetter = 'C'"
+    >
+      C
+    </button>
+  </nav>
+  <main>
+    <div class="letter letter-a" :class="{ 'letter-active': activeLetter === 'A' }">A</div>
+    <div class="letter letter-b" :class="{ 'letter-active': activeLetter === 'B' }">B</div>
+    <div class="letter letter-c" :class="{ 'letter-active': activeLetter === 'C' }">C</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'ColorBox',
+  data() {
+    return {
+      activeLetter: 'A'
+    }
+  }
+}
+</script>
+
+<style>
+main nav {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.btn {
+  font-size: 2rem;
+  display: inline-block;
+  margin-right: 10px;
+  width: 100px;
+  opacity: 0.3;
+  cursor: pointer;
+}
+
+.btn-active {
+  opacity: 1;
+}
+
+.btn-a {
+  background-color: hotpink;
+}
+
+.btn-b {
+  background-color: dodgerblue;
+}
+
+.btn-c {
+  background-color: mediumorchid;
+}
+
+main main {
+  text-align: center;
+}
+
+.letter {
+  opacity: 0.3;
+  display: inline-block;
+  padding: 30px 60px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 5rem;
+  margin-right: 10px;
+}
+
+.letter-active {
+  opacity: 1;
+}
+
+.letter-a {
+  color: hotpink;
+  border: 10px solid hotpink;
+}
+
+.letter-b {
+  color: dodgerblue;
+  border: 10px solid dodgerblue;
+}
+
+.letter-c {
+  color: mediumorchid;
+  border: 10px solid mediumorchid;
+}
+</style>
--- src/components/InfoBox.vue
+++ src/components/InfoBox.vue
@@ -3,12 +3,17 @@
     <span v-if="isVisible">Hide</span>
     <span v-else>Show</span>
   </button>
-  <div class="container" v-show="isVisible">Lorem ipsum.</div>
+  <div class="container" v-show="isVisible"><ColorBox /></div>
 </template>
 
 <script>
+import ColorBox from './ColorBox.vue'
+
 export default {
   name: 'InfoBox',
+  components: {
+    ColorBox
+  },
   data() {
     return {
       isVisible: true
