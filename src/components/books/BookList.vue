013657cd9435a7f826a9ae63109e05b3726ee248
Joe Ray Gregory <joe@may17.de>
2023-09-11 00:41:55 +0200

    Add task 10

---
+++ src/components/books/BookList.vue
@@ -0,0 +1,55 @@
+<template>
+  <table>
+    <BookListItem v-for="book in books" :key="book.isbn" :title="book.title" :isbn="book.isbn" />
+  </table>
+</template>
+<script>
+import BookListItem from './BookListItem.vue'
+
+export default {
+  name: 'BookList',
+  components: {
+    BookListItem
+  },
+  data() {
+    return {
+      books: []
+    }
+  },
+  created() {
+    fetch('http://localhost:4730/books')
+      .then((response) => response.json())
+      .then((jsonData) => (this.books = jsonData))
+  }
+}
+</script>
+
+<style>
+table {
+  font-family: 'Trebuchet MS', Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td,
+th {
+  border: 1px solid #dddddd;
+  padding: 8px;
+}
+
+th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4caf50;
+  color: white;
+}
+
+tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+tr:hover {
+  background-color: #dddddd;
+}
+</style>
---
+++ src/components/books/BookListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <tr>
+    <td>{{ title }}</td>
+    <td>{{ isbn }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: 'BookListItem',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    isbn: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
