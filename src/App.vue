2e4262b9043cd016918c35416db16252a10a713e
Joe Ray Gregory <joe@may17.de>
2023-09-10 21:46:33 +0200

    Add solution task 1

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,15 @@
 <template>
+  <AppNavigation />
   <h1>My first vue project</h1>
 </template>
+
+<script>
+import AppNavigation from './components/AppNavigation.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNavigation
+  }
+}
+</script>
