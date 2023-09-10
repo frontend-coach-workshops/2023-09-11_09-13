a10be747f280ac7b210443ff4d5131ce7357c420
Joe Ray Gregory <joe@may17.de>
2023-09-10 23:56:25 +0200

    Add task 5

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <AppNavigation />
+  <h1>My first vue project</h1>
   <BookList />
+  <MouseCursor />
+  <InfoBox />
 </template>
 
 <script>
 import AppNavigation from './components/AppNavigation.vue'
+import MouseCursor from './components/MouseCursor.vue'
+import InfoBox from './components/InfoBox.vue'
 import BookList from './components/BookList.vue'
 
 export default {
   name: 'App',
   components: {
     AppNavigation,
+    MouseCursor,
+    InfoBox,
     BookList
   }
 }
