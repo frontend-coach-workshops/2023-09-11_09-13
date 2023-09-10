e35e836f01e132f616ac0730c4a5e0bc6501c1f7
Joe Ray Gregory <joe@may17.de>
2023-09-10 23:02:05 +0200

    Add task 2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <AppNavigation />
   <h1>My first vue project</h1>
+  <MouseCursor />
 </template>
 
 <script>
 import AppNavigation from './components/AppNavigation.vue'
+import MouseCursor from './components/MouseCursor.vue'
 
 export default {
   name: 'App',
   components: {
-    AppNavigation
+    AppNavigation,
+    MouseCursor
   }
 }
 </script>
---
+++ src/components/MouseCursor.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="box">
+    <span>x:{{ x }}, y: {{ y }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MouseCursor',
+  data() {
+    return {
+      x: 12,
+      y: 4
+    }
+  }
+}
+</script>
+
+<style>
+#box {
+  height: 400px;
+  width: 400px;
+  border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
